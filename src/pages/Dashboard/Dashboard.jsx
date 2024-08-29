732910d9e7eda8785a66e856fb163a4a5a76a54d
adeeb jamil <adeebjamil6459@gmail.com>
2024-08-29 17:05:58 +0530

    Crypto-market

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,55 @@
+import DashboardLayout from "../../Components/DashboardLayout";
+import InfoCard from "./components/InfoCard";
+import PortfolioSection from "./components/PortfolioSection";
+import PriceSection from "./components/PriceSection";
+import Transactions from "./components/Transactions";
+import { Grid, GridItem } from "@chakra-ui/react";
+
+
+const Dashboard = ({}) => {
+  return (
+    <DashboardLayout title="Dashboard">
+      <Grid
+        gridTemplateColumns={{
+          base: "repeat(1, 1fr)",
+          xl: "repeat(2, 1fr)",
+        }}
+        gap="6"
+      >
+        <GridItem
+          colSpan={{
+            base: 1,
+            xl: 2,
+          }}
+        >
+          <PortfolioSection />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <PriceSection />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Transactions />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <InfoCard
+            imgUrl="/dot_bg.svg"
+            text=" Learn more about Loans – Keep your Bitcoin, access it’s value without
+          selling it"
+            tagText="Loan"
+            inverted={false}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <InfoCard
+            inverted={true}
+            tagText="Contact"
+            imgUrl="/grid_bg.svg"
+            text="Learn more about our real estate, mortgage, and  corporate account services"
+          />
+        </GridItem>
+      </Grid>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
